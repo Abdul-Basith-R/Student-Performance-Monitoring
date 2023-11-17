94ea6cd969b15ce299823007f79a57debb4d4a14
Abdul-Basith-R <abubas002@gmail,com>
2023-11-17 17:30:11 +0530

    Data Ingestion Done CHeck the logging and data path

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ import os
 
 LOG_FILE = f"{datetime.datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
 log_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makekdirs(log_path,exist_ok=True)
+os.makedirs(log_path, exist_ok=True)
 
 
 LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
@@ -13,5 +13,5 @@ LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.info,
+    level=logging.INFO,
 )
