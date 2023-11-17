40fd08f03b7e93c8bff54765be93cc3011d01d7e
Abdul-Basith-R <abubas002@gmail,com>
2023-11-17 10:48:14 +0530

    EDA And Model Training notebooks update

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import logging
+import datetime
+import os
+
+LOG_FILE = f"{datetime.datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makekdirs(log_path,exist_ok=True)
+
+
+LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.info,
+)
